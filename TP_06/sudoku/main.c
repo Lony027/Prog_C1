65ac447f4f34c0b79a54edeb92fc4271bf8fbfd5
Lony027 <iony@protonmail.com>
2025-01-21 16:11:05 +0100

    tp6

--- TP_06/sudoku/main.c
+++ TP_06/sudoku/main.c
@@ -1,60 +1,69 @@
-#include <stdio.h>
-
 #include "in_out.h"
 #include "sudoku.h"
 
-/*
-  -1 If new case create incoherence
-  1 if new case is coherent
- */
-int is_coherent(Board grid, int x, int y) {
-  int val = grid[x][y];
+int is_coherent(Board grid, int x, int y, int val) {
   int i;
   for (i = 0; i < 9; i++) {
     if (grid[i][y] == val || grid[x][i] == val) {
-      printf("l");
       return 0;
     }
   }
-  printf("LAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
+  int block_x = (x / 3) * 3;
+  int block_y = (y / 3) * 3;
+
+  int j;
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++) {
+      if (grid[block_x + i][block_y + j] == val) {
+        return 0;
+      }
+    }
+  }
+
   return 1;
 }
 
-void solve(Board grid, int current_x, int max, int y) {
+void solve(Board grid, int x, int y) {
+
+  if (x >= 9) {
+    solve(grid, 0, y + 1);
+    return;
+  }
+
   if (y >= 9) {
     print_board(grid);
     return;
   }
-  if (current_x >= max) {
+
+  if (grid[x][y] != 0) {
+    solve(grid, x + 1, y);
     return;
   }
 
-  int i, val;
-  for (i = 0; i < max; i++) {
-    val = grid[i][y];
-    if (val == 0) {
-      grid[i][y] = current_x;
-      if (is_coherent(grid, i, y)) {
-        solve(grid, current_x + 1, max, y);
-        /* val = 0; */
-      } else {
-        grid[i][y] = 0;
-      }
+  int i;
+  for (i = 1; i <= 9; i++) {
+    if (is_coherent(grid, x, y, i) == 1) {
+      grid[x][y] = i;
+      solve(grid, x + 1, y);
+      grid[x][y] = 0;
     }
   }
-  solve(grid, 1, max, y + 1);
 }
 
 int main(int argc, char *argv[]) {
   Board B;
 
-  fread_board(argv[1], B);
-  print_board(B);
+  int error = fread_board(argv[1], B);
+  if (error == 0) {
+    return -1;
+  }
+  /* print_board(B); */
 
-  initialize_empty_board(B);
-  print_board(B);
+  /* initialize_empty_board(B); */
+  /*   print_board(B); */
 
-  solve(B, 1, 9, 0);
-  print_board(B);
+  solve(B, 0, 0);
+  /* grid 5 à 7 solutions */
   return 0;
 }
--- TP_06/sudoku/sudoku.c
+++ TP_06/sudoku/sudoku.c
@@ -1,30 +1,63 @@
 #include <stdio.h>
 
 #include "sudoku.h"
-/* SIZE SHOULD BE DYNAMIC? */
 
-void initialize_empty_board(Board grid){
-    int i, j;
-    for (i = 0; i < 9; i++) {
-        for (j = 0; j < 9; j++) {
-            grid[i][j]=0;
-        }
+void initialize_empty_board(Board grid) {
+  int i, j;
+  for (i = 0; i < 9; i++) {
+    for (j = 0; j < 9; j++) {
+      grid[i][j] = 0;
     }
+  }
 }
 
-void print_board(Board grid){
-    int i, j, val;
-    for (i = 0; i < 9; i++) {
-        printf("-------------------------------------\n");
-        for (j = 0; j < 9; j++) {
-            val = grid[i][j];
-            if (val == 0) {
-                printf("|   ");
-            } else {
-                printf("| %d ", grid[i][j]);
-            }
-        }
-        printf("|\n");
+void print_empty_line() {
+  int i;
+  for (i = 0; i < 9; i++) {
+    if (i == 0 || i == 3 || i == 6) {
+      putchar('|');
     }
-     printf("-------------------------------------\n");
+    printf("|     ");
+  }
+  printf("||\n");
+}
+
+void print_line(Board grid, int y) {
+  int i, val;
+  for (i = 0; i < 9; i++) {
+    val = grid[y][i];
+    if (i == 0 || i == 3 || i == 6) {
+      putchar('|');
+    }
+
+    if (val == 0) {
+      printf("|     ");
+    } else {
+      printf("|  %d  ", val);
+    }
+  }
+  printf("||\n");
+}
+
+void print_simple_separator() {
+  printf("-----------------------------------------------------------\n");
+}
+
+void print_bold_separator() {
+  printf("===========================================================\n");
+}
+
+void print_board(Board grid) {
+  int i;
+  print_bold_separator();
+  for (i = 0; i < 9; i++) {
+    print_empty_line();
+    print_line(grid, i);
+    print_empty_line();
+    if (i == 2 || i == 5 || i == 8) {
+      print_bold_separator();
+    } else {
+      print_simple_separator();
+    }
+  }
 }
